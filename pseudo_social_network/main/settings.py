2b182e3689d606319f41f46d723262ada95ed265
asyl <asyliman20@gmail.com>
2025-03-07 17:12:36 +0600

    Добавьте для своего проекта документацию swagger

--- pseudo_social_network/main/settings.py
+++ pseudo_social_network/main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'djoser',
     'blog',
+    'drf_yasg',
     
 )
 
--- pseudo_social_network/main/urls.py
+++ pseudo_social_network/main/urls.py
@@ -4,11 +4,28 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])  
 def protected_resource(request):
     return JsonResponse({"message": "This is a protected resource."})
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Pseudo Social Network API",
+        default_version='v1',
+        description="Документация API для проекта Pseudo Social Network",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your_email@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,4 +36,6 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('protected-resource/', protected_resource),
     path('api/', include('blog.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
